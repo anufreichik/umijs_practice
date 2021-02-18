3981a3568128097133f2bae123574b391f221abd
anufreichik <marta149@hotmail.com>
2021-02-17 22:30:29 -0800

    counter modified

--- src/pages/counter/Counter.tsx
+++ src/pages/counter/Counter.tsx
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 import { connect } from 'umi';
 
 interface IProps {
  counter:Number;
  addOne:any;
+  minusOne:any;
 }
 
 
 function Counter(props:IProps) {
   const addOne=()=>{
     props.addOne(1);
+
+  }
+  const minusOne=()=>{
+    props.minusOne(1);
+
   }
   return (
     <div>
-      <h2>Counter</h2>
-      <button onClick={addOne}>Add One</button>
+      <button onClick={addOne}>+</button>
+     {props.counter}
+      <button onClick={minusOne}>-</button>
     </div>
   );
 }
 const mapStateToProps = (state: any) => ({
-  counter: state.counter,
+  counter: state.Counter.counter,
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
   addOne: (payload: Number) => dispatch({ type: 'Counter/addOne', payload }),
+  minusOne: (payload: Number) => dispatch({ type: 'Counter/minusOne', payload }),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/pages/counter/model.ts
+++ src/pages/counter/model.ts
@@ -10,10 +10,13 @@ export interface IModel {
   state: IState;
   effects: {
     addOne: Effect;
+    plusOne: Effect;
+    minusOne:Effect;
   };
   reducers: {
     save: Reducer<IState>;
     set: Reducer<IState>;
+    plusOne: Reducer<IState>;
   };
 }
 
@@ -23,14 +26,38 @@ const Model: IModel = {
   state: {counter:0},
 
   effects: {
+    //OPTION 1 getting state and then using save reducer
     *addOne({ payload }, { call, put }) {
       const state = yield select();
       yield put({ type: 'save', payload: {counter:state.Counter.counter + 1} });
     },
 
+
+    //OPTION 2 using reducer plusOne
+    *plusOne({ payload }, { call, put }) {
+      yield put({ type: 'plusOne' });
+    },
+
+
+    //OPTION 1 getting state and then using save reducer
+    *minusOne({ payload }, { call, put }) {
+      //const state = yield select();
+       const counterState = (state:any) => state.Counter;
+       const state = yield select(counterState);
+      yield put({ type: 'save', payload: {counter:state.counter - 1} });
+    },
+
   },
 
   reducers: {
+    //using reducer
+    plusOne(state: any, { payload }: any) {
+      return {
+        ...state,
+       counter: state.counter+1
+      };
+    },
+
     save(state: any, { payload }: any) {
       return {
         ...state,
